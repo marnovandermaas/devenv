b2748315fed0b0eb9559740ca04dad6c6af785b9
Marno van der Maas <mvdmaas+git@lowrisc.org>
2024-12-12 17:46:15 +0000

    Flake: add cache and ZSH

--- flake.nix
+++ flake.nix
@@ -13,6 +13,11 @@
     };
   };
 
+  nixConfig = {
+    extra-substituters = ["https://nix-cache.lowrisc.org/public/"];
+    extra-trusted-public-keys = ["nix-cache.lowrisc.org-public-1:O6JLD0yXzaJDPiQW1meVu32JIDViuaPtGDfjlOopU7o="];
+  };
+
   outputs = inputs @ {
     self,
     nixpkgs,
@@ -23,7 +28,7 @@
     devShells.x86_64-linux = {
       opentitan = inputs.lowrisc-nix.devShells.x86_64-linux.opentitan.override {
         edaTools = with inputs.lowrisc-it.packages.x86_64-linux; [vcs vivado xcelium];
-        extraPkgs = with inputs.nixpkgs.legacyPackages.x86_64-linux; [google-cloud-sdk rustup rust-analyzer libcap2];
+        extraPkgs = with inputs.nixpkgs.legacyPackages.x86_64-linux; [google-cloud-sdk rustup rust-analyzer zsh];
       };
     };
   };
